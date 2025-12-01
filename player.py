28f5fa345eb02f46df8f3c8f00578605d23b4ca0
lisa-gilot <lisa.gilot@edu.esiee.fr>
2025-12-01 15:07:50 +0000

    ajout historique

--- player.py
+++ player.py
@@ -32,6 +32,7 @@ class Player():
     def __init__(self, name):
         self.name = name
         self.current_room = None
+        self.history = []
     
     # Define the move method.
     def move(self, direction):
@@ -45,7 +46,18 @@ class Player():
         
         # Set the current room to the next room.
         self.current_room = next_room
+        self.history.append(next_room)
         print(self.current_room.get_long_description())
+        self.get_history()
         return True
+
+    def get_history(self):
+        if not self.history:
+            return
+        
+        print("\nVous avez déja visité les pièces suivantes:")
+        for room in self.history:
+            print(f"    - {room.name}")
+
 
     
